3269e574115dd843111cfa0d3afaa7988cddba08
philip993 <fitz993m@gmail.com>
2019-05-07 08:00:24 +0200

    ng-material separate module

---
+++ src/app/angular-material.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+import { MatToolbarModule } from "@angular/material";
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, MatToolbarModule],
+  exports: [CommonModule, MatToolbarModule]
+})
+export class AngularMaterialModule {}
