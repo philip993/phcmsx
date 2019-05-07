3ad7202ee7e308a2d55546afe1d916dea2089690
philip993 <fitz993m@gmail.com>
2019-05-07 12:56:45 +0200

    routes: View & Create

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { ViewComponent } from "./posts/view/view.component";
+import { CreateComponent } from "./posts/create/create.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "view", component: ViewComponent },
+  { path: "create", component: CreateComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
