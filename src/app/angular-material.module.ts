8d0e24e9679aee977f96dbb00ecb6c92a993bd93
philip993 <fitz993m@gmail.com>
2019-05-07 09:40:22 +0200

    imports

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 
-import { MatToolbarModule } from "@angular/material";
+import {
+  MatToolbarModule,
+  MatIconModule,
+  MatButtonModule
+} from "@angular/material";
 @NgModule({
   declarations: [],
   imports: [CommonModule, MatToolbarModule],
-  exports: [CommonModule, MatToolbarModule]
+  exports: [CommonModule, MatToolbarModule, MatIconModule, MatButtonModule]
 })
 export class AngularMaterialModule {}
